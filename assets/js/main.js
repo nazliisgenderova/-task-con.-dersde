062b50cbc6b06302f349bb043bc670b8acd8f3d5
Nurlan622 <7bxwn9m@code.edu.az>
2023-04-12 20:10:49 +0400

    task conditions

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+//   var num = 78;
+//  if(num>9 && num<100){
+//     var teklik=num%10
+//     var onluq=(num-teklik)/10
+
+//       if(teklik > onluq){
+//           alert("teklik boyukdur")
+//      }
+//      else{
+//           alert("onluq boyukdur")
+//       }
+//   }
+
+//  var num=2345;
+//  if(num>999 && num<10000){
+//     var teklik=num%10
+//      var onluq=((num-teklik)%100)/10
+//      var yuzluk=((num-num%100)/100)%10
+//      var minlik=(num-num%1000)/1000
+//      alert(teklik+onluq+yuzluk+minlik)
+//  }
+
+// var a=prompt("ay daxil edin")
+
+// switch(a) {
+//     case "yanvar":
+//     case "mart":
+//     case "may":
+//     case "iyul":
+//     case "avqust":
+//     case "oktyabr":
+//     case "dekabr":
+//     alert("31");
+//     break;
+
+//     case "fevral":
+//     alert("28-29");
+//     break;
+
+//     case "aprel":
+//     case "iyun":  
+//     case "sentyabr":
+//     case "noyabr":  
+//         alert("30");
+//         break;
+//     default:
+//     alert("bu ay qeyd olunmayib")
+//     break;
+// }
+
+// var sum=0
+// for(i=0; i<=100; i++){
+//     if(i%2==0){
+//      sum +=i
+//     }
+// }
+//  alert(sum)
